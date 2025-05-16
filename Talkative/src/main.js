843cd30bb70be994972a95620c1d2bd93c4692bd
Kush <kushsrigiri@gmail.com>
2025-05-16 13:19:49 +0200

    [docs]  | Files: Talkative/.gitignore, Talkative/index.html, Talkative/package-lock.json, Talkative/package.json, Talkative/public/vite.svg, Talkative/src/counter.js, Talkative/src/javascript.svg, Talkative/src/main.js, Talkative/src/style.css

---
+++ Talkative/src/main.js
@@ -0,0 +1,37 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+recognition.lang = 'en-US';
+recognition.continuous = false;
+
+recognition.onresult = (event) => {
+  const spokenWord = event.results[0][0].transcript.toLowerCase();
+  console.log("You said:", spokenWord);
+};
+
+document.body.onclick = () => {
+  recognition.start();
+};
